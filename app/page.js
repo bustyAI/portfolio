8c58063ddaac3805b905cccac36dba4dfe2275be
bustyAI <oscar120646@gmail.com>
2024-03-04 09:51:59 -0800

    adding more projects

--- app/page.js
+++ app/page.js
@@ -19,10 +19,13 @@ export default function Home() {
       </div>
 
       <div className='flex flex-row justify-evenly items-center flex-wrap text-2xl'>
-        <ProjectCard title={"Digital Library"} desc={"Browse books through our digital Library"} link={'https://github.com/bustyAI/Digital-Library'} linkBtn={'Browse Books'} animation={"flicker"} />
+        <ProjectCard title={"Vinfo"} desc={"Manage your fleet on our platform!"} link={"https://vinfo-brown.vercel.app/"} linkBtn={"Manage Fleet"} animation={'flicker4'} />
         <ProjectCard title="Renta Ride" desc={"Your go-to destination for hassle-free car rentals"} link={"https://renta-ride.vercel.app/"} linkBtn={'Rent a Ride'} animation={"flicker2"} />
+        <ProjectCard title={"Football Predictions"} desc={"Predicting Football match outcomes through Machine Learning"} link={"https://github.com/bustyAI/football-predictions"} linkBtn={"Predict"} animation={"flicker3"} />
+        <ProjectCard title={"Digital Library"} desc={"Browse books through our digital Library"} link={'https://github.com/bustyAI/Digital-Library'} linkBtn={'Browse Books'} animation={"flicker"} />
         <ProjectCard title={"BeSafe"} desc={"Protecting the Youth through spreading Awareness"} link={'https://github.com/bustyAI/besafe/tree/main'} linkBtn={"Stay Informed"} animation={"flicker3"} />
-        <ProjectCard title={"Jewelry Page"} desc={"Explore jewelry crafted by your beloved DIY artisan, Rotting Pumpkin."} link={'https://github.com/bustyAI/rotting-pumpkin'} linkBtn={"Browse Jewelry"} animation={"flicker4"} />
+        <ProjectCard title={"What Makes a Movie Great?"} desc={"Find out what makes a good movie, a good Movie!"} link={"https://github.com/bustyAI/Movie-Project"} linkBtn={"Come Find Out"} animation={"flicker4"} />
+
       </div>
 
     </main>
